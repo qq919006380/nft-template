02e37c7b2bd389123d627a33d4836873ccb16eff
weibaichao <919006380@qq.com>
2022-09-22 21:48:57 +0800

    ad

--- src/topbar/Login.js
+++ src/topbar/Login.js
@@ -14,70 +14,103 @@ const web3Modal = new Web3Modal({
   providerOptions,
 });
 
-const connectWallet = async () => {
-  console.log('connectWallet')
-  try {
-    // await web3Modal.clearCachedProvider();
-    const provider = await web3Modal.connect();
-    const library = new ethers.providers.Web3Provider(provider);
-  } catch (error) {
-    console.error(error);
-  }
-};
+
+
+
 export default function App() {
 
   // //组件内部定义入口按钮展示文案
-  const [currentAccount, setText] = useState(Store.currentAccount.get());
-
-  const [open, setOpen] = useState(false);
-
-  const handleMenuClick = (e) => {
-    if (e.key === '3') {
-      setOpen(false);
+  const [currentAccount, setCurrentAccount] = useState(Store.currentAccount.get());
+  const handleMenuClick = ({ key }) => {
+    console.log(key)
+    if (key == 2) {
+      handleDisconnect()
     }
   };
-
-  const handleOpenChange = (flag) => {
-    setOpen(flag);
-  };
-
   useEffect(() => {
     //使用mobx监听钱包状态的变化
     Store.currentAccount.observe_((newAccount) => {
-      setText(newAccount.newValue);
+      setCurrentAccount(newAccount.newValue);
     });
+    if (web3Modal.cachedProvider) {
+      connectWallet();
+    }
   }, []);
-  // 登陆
+  const connectWallet = async () => {
+    console.log('connectWallet')
+    try {
+      const provider = await web3Modal.connect();
+      const library = new ethers.providers.Web3Provider(provider);
+      const accounts = await library.listAccounts();
+      if (accounts) setCurrentAccount(accounts[0]);
+      //注册监听，比如disconnect，accountsChanged，chainChanged
+      registerEthListener(provider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleDisconnect = async () => {
+    console.log('disconnect')
+    // 刷新状态
+    setCurrentAccount();
+   
+    // 清除登录缓存
+    await web3Modal.clearCachedProvider();
+  };
+  const handleAccountsChanged = () => {
+    console.log('handleAccountsChanged')
+  }
+  const handleChainChanged = () => {
+    console.log('handleChainChanged')
+  }
+  //可以根据自己的需要些具体的内容
+  function registerEthListener(web3ModalProvider) {
+    web3ModalProvider
+      .on("disconnect", async (error) => {
+        handleDisconnect()
+      })
+      .on("accountsChanged", (accounts) => {
+        handleAccountsChanged()
+
+      })
+      .on("chainChanged", (chainId) => {
+        handleChainChanged()
+
+      });
+  }
+
+  // 点击登陆模块
   let LoginTemplate = () => {
     return <div onClick={connectWallet}>连接钱包</div>
   }
-  // 已连接钱包
+
+  // 已登录模块
   let ConnectedTemplate = () => {
     const menu = (
       <Menu
         onClick={handleMenuClick}
         items={[
           {
-            label: 'Clicking me will not close the menu.',
+
+            label: (
+              <a href="/list">
+                我的NFT
+              </a>
+            ),
             key: '1',
           },
           {
-            label: 'Clicking me will not close the menu also.',
-            key: '2',
-          },
-          {
             label: '退出登陆',
-            key: '3',
+            key: '2',
           },
         ]}
       />
     );
-    // <div onClick={connectWallet}>{truncateAddress(currentAccount)}</div>
-    return (<Dropdown overlay={menu} onOpenChange={handleOpenChange} open={open}>
+    return (<Dropdown trigger={['click']} overlay={menu}   >
       <a onClick={(e) => e.preventDefault()}>
         <Space>
-          Hover me
-          <DownOutlined />
+          <span className=" align-middle">{truncateAddress(currentAccount)}</span>
+          <span ><DownOutlined /></span>
         </Space>
       </a>
     </Dropdown>)
@@ -90,7 +123,6 @@ export default function App() {
   }
   return (
     <div className=" font-bold cursor-pointer hover:opacity-90">
-
       {template}
     </div>
 
